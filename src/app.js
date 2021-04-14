1b5db568e6821f13f0ec2878556c307d61e417a6
Martin Fuentes <mefuentes@uninorte.edu.co>
2021-04-13 23:54:28 -0500

    Se configura la connexión inicial con Firebird

--- src/app.js
+++ src/app.js
@@ -15,19 +15,31 @@ app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// middlewares
-app.use(morgan('dev'));
-app.use(myConnection(firebird, {
-	host: 'localhost',
-	user: 'SYSDBA',
-	password: 'root',
-	port: 3050,
-	database: 'BIENESTAR2'
-}, 'single'));
-app.use(express.urlencoded({ extended: true }));
+// // middlewares
+// app.use(morgan('dev'));
+// app.use(myConnection(firebird, {
+// 	host: 'localhost',
+// 	user: 'SYSDBA',
+// 	password: 'root',
+// 	port: 3050,
+// 	database: 'BIENESTAR2'
+// }, 'single'));
+// app.use(express.urlencoded({ extended: true }));
 
-// routes
+var options = {};
+ 
+options.host = '127.0.0.1';
+options.port = 3050;
+options.database = 'database.fdb';
+options.user = 'SYSDBA';
+options.password = 'root';
+options.lowercase_keys = false; // set to true to lowercase keys
+options.role = null;            // default
+options.pageSize = 4096;        // default when creating database
+
+//routes
 app.use('/', customerRoutes);
+
 
 // static files
 app.use(express.static('public'));
